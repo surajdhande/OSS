a26e16b929a34cd6f94d1a178dcf56d3f5c613ab
sonali <55425483+sonalibd9@users.noreply.github.com>
2022-07-08 15:33:19 +0530

    Add files via upload

---
+++ CustomersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRMWebApp.Models;
+namespace CRMWebApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            List<customer> c = customerManager.Lodedata();
+           // return this.Json(c, JsonRequestBehavior.AllowGet);
+            return View(c);
+        }
+    }
+}
